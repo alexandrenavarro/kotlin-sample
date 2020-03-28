10a64d991d587d2fb77f2f248ffce7c4dd32f8b3
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2020-03-28 23:14:16 +0100

    Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.6.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.71"
+	kotlin("plugin.spring") version "1.3.71"
+}
+
+group = "com.github.alexandrenavarro"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		//exclude(group = "org.mockito", module = "mockito-junit-jupiter")
+
+	}
+	//testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+}
+
+configurations.all {
+	resolutionStrategy {
+		failOnVersionConflict()
+		preferProjectModules()
+		dependencySubstitution {
+			substitute(module("org.junit.jupiter:junit-jupiter")).with(module("org.junit.jupiter:junit-jupiter:5.6.0"))
+		}
+	}
+}
+
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ src/test/kotlin/com/github/alexandrenavarro/kotlinsample/KotlinSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.alexandrenavarro.kotlinsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
